fd1fd0723dd37d50d3a9167b0ddf3285f0a912be
K Legg <kevindlegg@gmail.com>
2018-10-22 17:23:50 -0500

    initial ngOnInit setup

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
 
@@ -26,42 +26,45 @@ import { Passenger } from '../../models/passenger.interface';
     </div>
   `
 })
-export class PassengerDashboardComponent {
-  passengers: Passenger[] = [{
-    id: 1,
-    fullname: "Stephen",
-    checkedIn: true,
-    checkedInDate: 149742000000,
-    children: null
-  },
-  {
-    id: 2,
-    fullname: "Bobbi",
-    checkedIn: false,
-    checkedInDate: null,
-    children: [{ name: 'Chloe', age: 7 }]
-  },
-  {
-    id: 3,
-    fullname: "Shelbi",
-    checkedIn: true,
-    checkedInDate: 148742000000,
-    children: null
-  },
-  {
-    id: 4,
-    fullname: "Tucker",
-    checkedIn: true,
-    checkedInDate: 146742000000,
-    children: null
-  },
-  {
-    id: 5,
-    fullname: "Kevin",
-    checkedIn: false,
-    checkedInDate: null,
-    children: [{ name: 'Tucker', age: 21 },{ name: 'Shelbi', age: 24 }]
+export class PassengerDashboardComponent implements OnInit {
+  passengers: Passenger[];
+  constructor() {}
+  ngOnInit() {
+    console.log('ngOnInit');
+    this.passengers  = [{
+      id: 1,
+      fullname: "Stephen",
+      checkedIn: true,
+      checkedInDate: 149742000000,
+      children: null
+    },
+    {
+      id: 2,
+      fullname: "Bobbi",
+      checkedIn: false,
+      checkedInDate: null,
+      children: [{ name: 'Chloe', age: 7 }]
+    },
+    {
+      id: 3,
+      fullname: "Shelbi",
+      checkedIn: true,
+      checkedInDate: 148742000000,
+      children: null
+    },
+    {
+      id: 4,
+      fullname: "Tucker",
+      checkedIn: true,
+      checkedInDate: 146742000000,
+      children: null
+    },
+    {
+      id: 5,
+      fullname: "Kevin",
+      checkedIn: false,
+      checkedInDate: null,
+      children: [{ name: 'Tucker', age: 21 },{ name: 'Shelbi', age: 24 }]
+    }]
   }
-  ]
-
 }
