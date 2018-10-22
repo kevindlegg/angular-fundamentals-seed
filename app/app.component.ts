578dfde7017c57b647642befddb1b1ab2202ed2a
K Legg <kevindlegg@gmail.com>
2018-10-22 15:40:58 -0500

    passenger iteration 1

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,65 @@
 import { Component } from '@angular/core';
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+        <span 
+          class="status"
+          [class.checked-in]="passenger.checkedIn"></span>
+          {{ i + 1 }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+        <span 
+          class="status"
+          [ngClass]="{
+            'checked-in': passenger.checkedIn,
+            'checked-out': !passenger.checkedIn
+          }"></span>
+          {{ i + 1 }}: {{ passenger.fullname }}
+        </li>
+      </ul>
     </div>
   `
 })
-export class AppComponent {
 
+export class AppComponent {
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: "Stephen",
+    checkedIn: true
+  },
+  {
+    id: 2,
+    fullname: "Bobbi",
+    checkedIn: false
+  },
+  {
+    id: 3,
+    fullname: "Shelbi",
+    checkedIn: true
+  },
+  {
+    id: 4,
+    fullname: "Tucker",
+    checkedIn: true
+  },
+  {
+    id: 5,
+    fullname: "Kevin",
+    checkedIn: false
+  }
+  ]
 }
