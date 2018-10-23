0f5e25ac4f8deecaa931d97f738eaedd5a19fd32
K Legg <kevindlegg@gmail.com>
2018-10-23 14:28:05 -0500

    output and eventemitter changes

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -60,10 +60,18 @@ export class PassengerDashboardComponent implements OnInit {
       children: [{ name: 'Tucker', age: 21 },{ name: 'Shelbi', age: 24 }]
     }]
   }
-  handleEdit(event) {
-    console.log('Edit:', event)
+  handleEdit(event: Passenger) {
+    this.passengers = this.passengers.map((passenger: Passenger) => {
+      if (passenger.id === event.id) {
+        passenger = Object.assign({}, passenger, event);
+      }
+      return passenger;
+    });
+    console.log(this.passengers);
   }
-  handleRemove(event) {
-    console.log('Remove:', event)
+  handleRemove(event: Passenger) {
+    this.passengers = this.passengers.filter((passenger: Passenger) => {
+      return passenger.id !== event.id;
+    });
   }
 }
