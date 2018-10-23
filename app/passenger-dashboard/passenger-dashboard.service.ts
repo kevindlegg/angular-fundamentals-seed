8a735cfad90df82986b0b44cb3f349643d54eb6f
K Legg <kevindlegg@gmail.com>
2018-10-23 15:33:48 -0500

    Http put, delete with immutable state

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,47 +1,32 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 import { Passenger } from './models/passenger.interface';
+
+const PASSENGER_API: string = '/api/passengers';
 
 @Injectable()
 export class PassengerDashboardService {
   constructor(private http: Http) {}
 
-  getPassengers(): Passenger[] {
-    return [{
-      id: 1,
-      fullname: "Stephen",
-      checkedIn: true,
-      checkedInDate: 149742000000,
-      children: null
-    },
-    {
-      id: 2,
-      fullname: "Bobbi",
-      checkedIn: false,
-      checkedInDate: null,
-      children: [{ name: 'Chloe', age: 7 }]
-    },
-    {
-      id: 3,
-      fullname: "Shelbi",
-      checkedIn: true,
-      checkedInDate: 148742000000,
-      children: null
-    },
-    {
-      id: 4,
-      fullname: "Tucker",
-      checkedIn: true,
-      checkedInDate: 146742000000,
-      children: null
-    },
-    {
-      id: 5,
-      fullname: "Kevin",
-      checkedIn: false,
-      checkedInDate: null,
-      children: [{ name: 'Tucker', age: 21 },{ name: 'Shelbi', age: 24 }]
-    }]
+  getPassengers(): Observable<Passenger[]> {
+    return this.http
+      .get(PASSENGER_API)
+      .map((response: Response) => response.json());
+  }
+
+  updatePassenger(passenger: Passenger): Observable<Passenger> {
+    return this.http
+      .put(`${PASSENGER_API}/${passenger.id}`, passenger)
+      .map((response: Response) => response.json());
+  }
+
+  removePassenger(passenger: Passenger): Observable<Passenger> {
+    return this.http
+      .delete(`${PASSENGER_API}/${passenger.id}`)
+      .map((response: Response) => response.json());
   }
 }
