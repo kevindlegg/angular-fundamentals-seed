fb427049f4cde4cf1fbad3b3a73d40838c55cfe7
K Legg <kevindlegg@gmail.com>
2018-10-24 13:57:52 -0500

    route params, data-fetching with switchMap, imperative routing API

--- app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -28,6 +28,9 @@ import { Passenger } from '../../models/passenger.interface';
       <button (click)="onRemove()">
         Remove
       </button>
+      <button (click)="goToPassenger()">
+        View
+      </button>
     </div>
   `
 })
@@ -36,10 +39,13 @@ export class PassengerDetailComponent implements OnChanges {
   detail: Passenger;
 
   @Output()
-  edit: EventEmitter<any> = new EventEmitter();
+  edit: EventEmitter<Passenger> = new EventEmitter<Passenger>();
 
   @Output()
-  remove: EventEmitter<any> = new EventEmitter();
+  remove: EventEmitter<Passenger> = new EventEmitter<Passenger>();
+
+  @Output()
+  view: EventEmitter<Passenger> = new EventEmitter<Passenger>();
 
   editing: boolean = false;
 
@@ -53,6 +59,10 @@ export class PassengerDetailComponent implements OnChanges {
 
   onNameChange(value: string) {
     this.detail.fullname = value;
+  }
+
+  goToPassenger() {
+    this.view.emit(this.detail);
   }
 
   onRemove(value: string) {
